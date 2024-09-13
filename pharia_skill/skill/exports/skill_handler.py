333fa97b3831bc60d79dee20a52a8e802e74f144
peter.barth <peter.barth@aleph-alpha.com>
2024-09-13 18:02:32 +0200

    feat: add the skill.wit and the generated bindings to the module

---
+++ pharia_skill/skill/exports/skill_handler.py
@@ -0,0 +1,26 @@
+from typing import TypeVar, Generic, Union, Optional, Protocol, Tuple, List, Any, Self
+from enum import Flag, Enum, auto
+from dataclasses import dataclass
+from abc import abstractmethod
+import weakref
+
+from ..types import Result, Ok, Err, Some
+
+
+
+@dataclass
+class Error_Internal:
+    value: str
+
+
+@dataclass
+class Error_InvalidInput:
+    value: str
+
+
+Error = Union[Error_Internal, Error_InvalidInput]
+"""
+The set of errors which may be raised by functions in this interface
+"""
+
+
