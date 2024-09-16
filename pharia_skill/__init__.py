e90a2d96c5434048391788117c767e9095ba5210
peter.barth <peter.barth@aleph-alpha.com>
2024-09-16 14:46:01 +0200

    feat: correct error type if skill handling fails

--- pharia_skill/__init__.py
+++ pharia_skill/__init__.py
@@ -1,6 +1,8 @@
 import json
 from typing import Callable
 from .wit import exports
+from .wit.exports.skill_handler import Error_Internal
+from .wit.types import Err
 
 
 def skill(func: Callable) -> Callable:
@@ -8,8 +10,8 @@ def skill(func: Callable) -> Callable:
         def run(self, input: bytes) -> bytes:
             try:
                 return func(input)
-            except Exception as e:
-                return json.dumps(str(e)).encode()
+            except Exception as exc:
+                raise Err(Error_Internal(str(exc)))
 
     func.__globals__["SkillHandler"] = SkillHandler
     return func
