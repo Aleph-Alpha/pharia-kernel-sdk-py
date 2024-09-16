7c140a00fd267023ad600a06bb72c23d2332f0f7
Jia Ho Lee <jiaho.lee@aleph-alpha.com>
2024-09-16 16:58:51 +0200

    style: lint

--- tests/decorator_test.py
+++ tests/decorator_test.py
@@ -1,12 +1,14 @@
 import pytest
 from pharia_skill import skill
 
+
 def test_raise_error_if_two_skills_defined():
     @skill
     def foo():
         pass
 
     with pytest.raises(AssertionError):
+
         @skill
         def bar():
             pass
