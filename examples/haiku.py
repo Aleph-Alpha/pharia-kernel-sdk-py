6ca6f8234868b5527633b9b1742d41c7481cee2c
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-09-17 16:43:14 +0200

    feat: use llama 3.1 prompt syntax in example

--- examples/haiku.py
+++ examples/haiku.py
@@ -9,7 +9,11 @@ class MyModel(BaseModel):
 
 @skill
 def haiku(csi: Csi, input: MyModel) -> str:
-    prompt = f"""Write a haiku about {input.topic}"""
+    prompt = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+    You are a poet who strictly speaks in haikus.<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+    {input.topic}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
     params = CompletionParams(10, None, None, None, [])
     completion = csi.complete("llama-3.1-8b-instruct", prompt, params)
     return completion.text
