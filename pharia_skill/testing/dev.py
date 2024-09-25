22de8105173ac8227260b99c2b5b811cdfb38be7
Jia Ho Lee <jiaho.lee@aleph-alpha.com>
2024-09-25 18:49:42 +0200

    refactor: implement remote CSI for v0.2 fully

--- pharia_skill/testing/dev.py
+++ pharia_skill/testing/dev.py
@@ -23,7 +23,7 @@ class _DevCsi(Csi):
 
     def __init__(self):
         load_dotenv()
-        self.url = os.environ["PHARIA_KERNEL_CSI_ADDRESS"]
+        self.url = os.environ["PHARIA_KERNEL_CSI_ADDRESS"] + "/csi"
         token = os.environ["AA_API_TOKEN"]
         self.session = requests.Session()
         self.session.headers = {"Authorization": f"Bearer {token}"}
@@ -31,7 +31,9 @@ class _DevCsi(Csi):
     def __del__(self):
         self.session.close()
 
-    def complete(self, model: str, prompt: str, params: CompletionParams) -> Completion:
+    def complete(
+        self, model: str, prompt: str, params: CompletionParams
+    ) -> Completion:
         data = {
             "version": self.VERSION,
             "function": self.complete.__name__,
@@ -44,10 +46,43 @@ class _DevCsi(Csi):
         return Completion(**response.json())
 
     def chunk(self, text: str, params: ChunkParams) -> list[str]:
-        raise NotImplementedError
+        data = {
+            "version": self.VERSION,
+            "function": self.chunk.__name__,
+            "text": text,
+            "params": asdict(params),
+        }
+        response = self.session.post(self.url, json=data)
+        response.raise_for_status()
+        return response.json()
 
-    def select_language(self, text: str, languages: list[Language]) -> Language | None:
-        raise NotImplementedError
+    def select_language(
+        self, text: str, languages: list[Language]
+    ) -> Language | None:
+        data = {
+            "version": self.VERSION,
+            "function": self.select_language.__name__,
+            "text": text,
+            "languages": [language.name.lower() for language in languages],
+        }
+        response = self.session.post(self.url, json=data)
+        response.raise_for_status()
+        match response.json():
+            case "eng":
+                return Language.ENG
+            case "deu":
+                return Language.DEU
+            case _:
+                return None
 
-    def complete_all(self, requests: list[CompletionRequest]) -> list[Completion]:
-        raise NotImplementedError
+    def complete_all(
+        self, requests: list[CompletionRequest]
+    ) -> list[Completion]:
+        data = {
+            "version": self.VERSION,
+            "function": self.complete_all.__name__,
+            "requests": [asdict(request) for request in requests],
+        }
+        response = self.session.post(self.url, json=data)
+        response.raise_for_status()
+        return [Completion(**completion) for completion in response.json()]
