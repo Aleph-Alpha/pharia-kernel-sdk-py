07f1eed90eadd35427c2d6eaa437a30f1fc19960
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-10-01 14:03:59 +0200

    feat: log result of skill publishing to stdout

--- pharia_skill/cli.py
+++ pharia_skill/cli.py
@@ -5,6 +5,12 @@ import subprocess
 
 from dotenv import load_dotenv
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
 logger = logging.getLogger(__name__)
 
 
