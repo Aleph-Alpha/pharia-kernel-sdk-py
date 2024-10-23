3e41ab658c5324c420ac7d0fb5aeb0844720c227
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-10-23 17:07:04 +0200

    test: move model to fixture

--- tests/testing/dev_test.py
+++ tests/testing/dev_test.py
@@ -18,25 +18,30 @@ def csi() -> DevCsi:
     return DevCsi()
 
 
+@pytest.fixture(scope="module")
+def model() -> str:
+    return "llama-3.1-8b-instruct"
+
+
 @pytest.mark.kernel
-def test_complete(csi: DevCsi):
+def test_complete(csi: DevCsi, model: str):
     params = CompletionParams(max_tokens=64)
-    result = csi.complete("llama-3.1-8b-instruct", "Say hello to Bob", params)
+    result = csi.complete(model, "Say hello to Bob", params)
     assert "Bob" in result.text
 
 
 @pytest.mark.kernel
-def test_chat(csi: DevCsi):
+def test_chat(csi: DevCsi, model: str):
     params = ChatParams(max_tokens=64)
     messages = [Message(role=Role.USER, content="Say hello to Bob")]
-    result = csi.chat("llama-3.1-8b-instruct", messages, params)
+    result = csi.chat(model, messages, params)
     assert "Bob" in result.message.content
 
 
 @pytest.mark.kernel
-def test_chunk(csi: DevCsi):
+def test_chunk(csi: DevCsi, model: str):
     text = "A very very very long text that can be chunked."
-    params = ChunkParams(model="llama-3.1-8b-instruct", max_tokens=1)
+    params = ChunkParams(model, max_tokens=1)
     result = csi.chunk(text, params)
     assert len(result) == 13
 
@@ -50,10 +55,10 @@ def test_select_language(csi: DevCsi):
 
 
 @pytest.mark.kernel
-def test_complete_all(csi: DevCsi):
+def test_complete_all(csi: DevCsi, model: str):
     params = CompletionParams(max_tokens=64)
-    request_1 = CompletionRequest("llama-3.1-8b-instruct", "Say hello to Alice", params)
-    request_2 = CompletionRequest("llama-3.1-8b-instruct", "Say hello to Bob", params)
+    request_1 = CompletionRequest(model, "Say hello to Alice", params)
+    request_2 = CompletionRequest(model, "Say hello to Bob", params)
     result = csi.complete_all([request_1, request_2])
     assert len(result) == 2
     assert "Alice" in result[0].text
