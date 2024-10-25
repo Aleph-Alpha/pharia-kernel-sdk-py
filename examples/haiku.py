76be8175d0360521435cefe3ec92444b455d40a0
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-10-25 10:34:39 +0200

    test: add chat call to haiku for integration testing

--- examples/haiku.py
+++ examples/haiku.py
@@ -1,6 +1,10 @@
+"""
+Call into the completion and chat completion interface to test both in WASM
+"""
+
 from pydantic import BaseModel
 
-from pharia_skill import CompletionParams, Csi, skill
+from pharia_skill import ChatParams, CompletionParams, Csi, Message, skill
 
 
 class Input(BaseModel):
@@ -8,7 +12,7 @@ class Input(BaseModel):
 
 
 @skill
-def haiku(csi: Csi, input: Input) -> str:
+def haiku(csi: Csi, input: Input) -> dict[str, str]:
     prompt = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
 
     You are a poet who strictly speaks in haikus.<|eot_id|><|start_header_id|>user<|end_header_id|>
@@ -16,4 +20,15 @@ def haiku(csi: Csi, input: Input) -> str:
     {input.topic}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
     params = CompletionParams(max_tokens=64)
     completion = csi.complete("llama-3.1-8b-instruct", prompt, params)
-    return completion.text.strip()
+
+    # same prompt as above but for the chat completion
+    msg = Message.user(
+        f"You are a poet who strictly speaks in haikus.\n\n{input.topic}"
+    )
+    chat_completion = csi.chat(
+        "llama-3.1-8b-instruct", [msg], ChatParams(max_tokens=64)
+    )
+    return {
+        "completion": completion.text.strip(),
+        "chat": chat_completion.message.content.strip(),
+    }
