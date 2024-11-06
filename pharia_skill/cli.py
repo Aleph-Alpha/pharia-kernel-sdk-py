9a38e46715eb4d4883aa9155feffcfee29306132
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-11-06 15:16:31 +0100

    feat: use pharia-skill-cli jfrog binary

--- pharia_skill/cli.py
+++ pharia_skill/cli.py
@@ -3,8 +3,9 @@ import logging
 import os
 import subprocess
 
-import requests
 from dotenv import load_dotenv
+
+from .pharia_skill_cli import PhariaSkillCli
 
 logging.basicConfig(
     level=logging.INFO,
@@ -70,96 +71,6 @@ def image_exists(image_name: str) -> bool:
     return image_name in result.stdout
 
 
-def publish(skill: str):
-    """Publish a skill to an OCI registry.
-
-    Takes a path to a WASM component, wrap it in an OCI image and publish it to an OCI
-    registry under the `latest` tag. This does not fully deploy the skill, as an older
-    version might still be cached in the Kernel.
-    """
-    try:
-        jfrog_user = os.environ["JFROG_USER"]
-        jfrog_password = os.environ["JFROG_PASSWORD"]
-        skill_registry_user = os.environ["SKILL_REGISTRY_USER"]
-        skill_registry_password = os.environ["SKILL_REGISTRY_PASSWORD"]
-        skill_registry = os.environ["SKILL_REGISTRY"]
-        skill_repository = os.environ["SKILL_REPOSITORY"]
-    except KeyError as e:
-        logger.error(f"Environment variable {e} is not set.")
-        return
-
-    image = "alephalpha.jfrog.io/pharia-kernel-images/pharia-skill:latest"
-
-    if not image_exists(image):
-        logger.info("Pulling pharia-skill image...")
-
-        subprocess.run(
-            [
-                "podman",
-                "login",
-                "alephalpha.jfrog.io/pharia-kernel-images",
-                "-u",
-                jfrog_user,
-                "-p",
-                jfrog_password,
-            ],
-            check=True,
-        )
-
-        subprocess.run(["podman", "pull", image], check=True)
-        subprocess.run(["podman", "tag", image, "pharia-skill"], check=True)
-
-    # add file extension
-    if not skill.endswith(".wasm"):
-        skill += ".wasm"
-
-    # allow relative paths
-    if not skill.startswith(("/", "./")):
-        skill = f"./{skill}"
-
-    container_path = "/" + skill.split("/")[-1]
-    volume = f"{skill}:{container_path}"
-    command = [
-        "podman",
-        "run",
-        "-v",
-        volume,
-        "pharia-skill",
-        "publish",
-        "-R",
-        skill_registry,
-        "-r",
-        skill_repository,
-        "-u",
-        skill_registry_user,
-        "-p",
-        skill_registry_password,
-        "-t",
-        "latest",
-        container_path,
-    ]
-    subprocess.run(command, check=True)
-    logger.info("Skill published successfully.")
-
-
-def invalidate_cache(skill: str):
-    try:
-        kernel_address = os.environ["PHARIA_KERNEL_ADDRESS"]
-        token = os.environ["AA_API_TOKEN"]
-        namespace = os.environ["SKILL_NAMESPACE"]
-    except KeyError as e:
-        logger.error(f"Environment variable {e} is not set.")
-        return
-
-    url = f"{kernel_address}/cached_skills/{namespace}%2f{skill}"
-    headers = {"Authorization": f"Bearer {token}"}
-    logger.info("Invalidating cache...")
-    response = requests.delete(url, headers=headers)
-    if response.status_code not in (200, 204):
-        raise Exception(f"{response.status_code}: {response.text}")
-    logger.info(response.json())
-
-
 def main():
     load_dotenv()
 
@@ -181,8 +92,8 @@ def main():
             setup_wasi_deps()
             run_componentize_py(args.skill)
         elif args.command == "publish":
-            publish(args.skill)
-            invalidate_cache(args.skill)
+            cli = PhariaSkillCli()
+            cli.publish(args.skill)
         else:
             parser.print_help()
     except subprocess.CalledProcessError as e:
