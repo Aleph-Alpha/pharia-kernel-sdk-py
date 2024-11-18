44ff2d8d4373c5ec1709e8cfb609f2fa8f16eeaf
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-11-18 19:41:48 +0000

    docs: use double quotes in code example multi line string

--- pharia_skill/csi.py
+++ pharia_skill/csi.py
@@ -234,11 +234,11 @@ class Csi(Protocol):
 
         Examples::
 
-            prompt = f'''<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+            prompt = f\"\"\"<|begin_of_text|><|start_header_id|>system<|end_header_id|>
 
             You are a poet who strictly speaks in haikus.<|eot_id|><|start_header_id|>user<|end_header_id|>
 
-            {input.root}<|eot_id|><|start_header_id|>assistant<|end_header_id|>'''
+            {input.root}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\"\"\"
             params = CompletionParams(max_tokens=64)
             completion = csi.complete("llama-3.1-8b-instruct", prompt, params)
         """
