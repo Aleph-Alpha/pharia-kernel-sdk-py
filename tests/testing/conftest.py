3c68ed85c06ee3b258fa32a8fcfb053ad269a5ee
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-11-19 13:00:04 +0000

    test: fixture export readable span

--- tests/testing/conftest.py
+++ tests/testing/conftest.py
@@ -47,109 +47,121 @@ def exporter(provider: TracerProvider) -> InMemorySpanExporter:
 
 
 @pytest.fixture
-def inner_span() -> dict:
-    return {
-        "name": "complete",
-        "context": {
-            "trace_id": "0xbf1bd6a9821572d268a6b1624b2db5ed",
-            "span_id": "0x61aa9d8872df195e",
-            "trace_state": "[]",
-        },
-        "kind": "SpanKind.INTERNAL",
-        "parent_id": "0x0518819f195c3f15",
-        "start_time": "2024-11-14T13:17:13.381386Z",
-        "end_time": "2024-11-14T13:17:14.485530Z",
-        "status": {"status_code": "OK"},
-        "attributes": {
-            "type": "TASK_SPAN",
-            "input": '{"prompt": "oat milk", "model": "llama-3.1-8b-instruct", "params": {"max_tokens": 64, "temperature": null, "top_k": null, "top_p": null, "stop": []}, "version": "0.2", "function": "complete"}',
-            "output": '{"finish_reason": "length", "text": ", and other plant-based milks. The company also offers a range of dairy-free yogurt alternatives made from coconut milk, almond milk, and other plant-based milks.\\nIn addition to its dairy-free products, So Delicious also offers a range of other products, including:\\n* Frozen desserts: So Delicious offers a range of"}',
-        },
-        "events": [],
-        "links": [],
-        "resource": {
-            "attributes": {
-                "telemetry.sdk.language": "python",
-                "telemetry.sdk.name": "opentelemetry",
-                "telemetry.sdk.version": "1.28.1",
-                "service.name": "unknown_service",
+def inner_span() -> ReadableSpan:
+    """A children span to `outer_span`."""
+    return from_json(
+        {
+            "name": "complete",
+            "context": {
+                "trace_id": "0xbf1bd6a9821572d268a6b1624b2db5ed",
+                "span_id": "0x61aa9d8872df195e",
+                "trace_state": "[]",
             },
-            "schema_url": "",
-        },
-    }
-
-
-@pytest.fixture
-def outer_span() -> dict:
-    return {
-        "name": "haiku",
-        "context": {
-            "trace_id": "0xbf1bd6a9821572d268a6b1624b2db5ed",
-            "span_id": "0x0518819f195c3f15",
-            "trace_state": "[]",
-        },
-        "kind": "SpanKind.INTERNAL",
-        "parent_id": None,
-        "start_time": "2024-11-14T13:17:13.381318Z",
-        "end_time": "2024-11-14T13:17:14.485625Z",
-        "status": {"status_code": "OK"},
-        "attributes": {
-            "type": "TASK_SPAN",
-            "input": '{"input": "oat milk"}',
-            "output": '{"output": ", and other plant-based milks. The company also offers a range of dairy-free yogurt alternatives made from coconut milk, almond milk, and other plant-based milks.\\nIn addition to its dairy-free products, So Delicious also offers a range of other products, including:\\n* Frozen desserts: So Delicious offers a range of"}',
-        },
-        "events": [],
-        "links": [],
-        "resource": {
+            "kind": "SpanKind.INTERNAL",
+            "parent_id": "0x0518819f195c3f15",
+            "start_time": "2024-11-14T13:17:13.381386Z",
+            "end_time": "2024-11-14T13:17:14.485530Z",
+            "status": {"status_code": "OK"},
             "attributes": {
-                "telemetry.sdk.language": "python",
-                "telemetry.sdk.name": "opentelemetry",
-                "telemetry.sdk.version": "1.28.1",
-                "service.name": "unknown_service",
+                "type": "TASK_SPAN",
+                "input": '{"prompt": "oat milk", "model": "llama-3.1-8b-instruct", "params": {"max_tokens": 64, "temperature": null, "top_k": null, "top_p": null, "stop": []}, "version": "0.2", "function": "complete"}',
+                "output": '{"finish_reason": "length", "text": ", and other plant-based milks. The company also offers a range of dairy-free yogurt alternatives made from coconut milk, almond milk, and other plant-based milks.\\nIn addition to its dairy-free products, So Delicious also offers a range of other products, including:\\n* Frozen desserts: So Delicious offers a range of"}',
             },
-            "schema_url": "",
-        },
-    }
-
-
-@pytest.fixture
-def error_span() -> dict:
-    return {
-        "name": "haiku",
-        "context": {
-            "trace_id": "0x42b14b8889b0b6b79d0decfb21e2254f",
-            "span_id": "0x844bce85fb833055",
-            "trace_state": "[]",
-        },
-        "kind": "SpanKind.INTERNAL",
-        "parent_id": None,
-        "start_time": "2024-11-17T21:18:33.283500Z",
-        "end_time": "2024-11-17T21:18:33.285685Z",
-        "status": {"status_code": "ERROR", "description": "ValueError: out of cheese"},
-        "attributes": {"type": "TASK_SPAN", "input": "{}"},
-        "events": [
-            {
-                "name": "exception",
-                "timestamp": "2024-11-17T21:18:33.285586Z",
+            "events": [],
+            "links": [],
+            "resource": {
                 "attributes": {
-                    "exception.type": "ValueError",
-                    "exception.message": "out of cheese",
-                    "exception.stacktrace": "Traceback (most recent call last):",
-                    "exception.escaped": "False",
+                    "telemetry.sdk.language": "python",
+                    "telemetry.sdk.name": "opentelemetry",
+                    "telemetry.sdk.version": "1.28.1",
+                    "service.name": "unknown_service",
                 },
-            }
-        ],
-        "links": [],
-        "resource": {
-            "attributes": {
-                "telemetry.sdk.language": "python",
-                "telemetry.sdk.name": "opentelemetry",
-                "telemetry.sdk.version": "1.28.1",
-                "service.name": "unknown_service",
+                "schema_url": "",
             },
-            "schema_url": "",
-        },
-    }
+        }
+    )
+
+
+@pytest.fixture
+def outer_span() -> ReadableSpan:
+    """A parent span to `inner_span`."""
+    return from_json(
+        {
+            "name": "haiku",
+            "context": {
+                "trace_id": "0xbf1bd6a9821572d268a6b1624b2db5ed",
+                "span_id": "0x0518819f195c3f15",
+                "trace_state": "[]",
+            },
+            "kind": "SpanKind.INTERNAL",
+            "parent_id": None,
+            "start_time": "2024-11-14T13:17:13.381318Z",
+            "end_time": "2024-11-14T13:17:14.485625Z",
+            "status": {"status_code": "OK"},
+            "attributes": {
+                "type": "TASK_SPAN",
+                "input": '{"input": "oat milk"}',
+                "output": '{"output": ", and other plant-based milks. The company also offers a range of dairy-free yogurt alternatives made from coconut milk, almond milk, and other plant-based milks.\\nIn addition to its dairy-free products, So Delicious also offers a range of other products, including:\\n* Frozen desserts: So Delicious offers a range of"}',
+            },
+            "events": [],
+            "links": [],
+            "resource": {
+                "attributes": {
+                    "telemetry.sdk.language": "python",
+                    "telemetry.sdk.name": "opentelemetry",
+                    "telemetry.sdk.version": "1.28.1",
+                    "service.name": "unknown_service",
+                },
+                "schema_url": "",
+            },
+        }
+    )
+
+
+@pytest.fixture
+def error_span() -> ReadableSpan:
+    """A root level span with an error status. Not related to any other span."""
+    return from_json(
+        {
+            "name": "haiku",
+            "context": {
+                "trace_id": "0x42b14b8889b0b6b79d0decfb21e2254f",
+                "span_id": "0x844bce85fb833055",
+                "trace_state": "[]",
+            },
+            "kind": "SpanKind.INTERNAL",
+            "parent_id": None,
+            "start_time": "2024-11-17T21:18:33.283500Z",
+            "end_time": "2024-11-17T21:18:33.285685Z",
+            "status": {
+                "status_code": "ERROR",
+                "description": "ValueError: out of cheese",
+            },
+            "attributes": {"type": "TASK_SPAN", "input": "{}"},
+            "events": [
+                {
+                    "name": "exception",
+                    "timestamp": "2024-11-17T21:18:33.285586Z",
+                    "attributes": {
+                        "exception.type": "ValueError",
+                        "exception.message": "out of cheese",
+                        "exception.stacktrace": "Traceback (most recent call last):",
+                        "exception.escaped": "False",
+                    },
+                }
+            ],
+            "links": [],
+            "resource": {
+                "attributes": {
+                    "telemetry.sdk.language": "python",
+                    "telemetry.sdk.name": "opentelemetry",
+                    "telemetry.sdk.version": "1.28.1",
+                    "service.name": "unknown_service",
+                },
+                "schema_url": "",
+            },
+        }
+    )
 
 
 def status_str_to_status(status_str: str) -> StatusCode:
