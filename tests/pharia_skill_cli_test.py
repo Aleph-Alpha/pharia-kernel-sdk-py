ca9aa0ff5191204bc05e6864d6000c9e6840c367
peter.barth <peter.barth@aleph-alpha.com>
2024-11-20 11:40:26 +0100

    ci: update test workflow permissions and env
    Update test workflow to include permissions and environment variables
    for Artifactory URL and API token.

--- tests/pharia_skill_cli_test.py
+++ tests/pharia_skill_cli_test.py
@@ -1,29 +1,14 @@
-import os
-
 import pytest
 
-from pharia_skill.cli import run_componentize_py, setup_wasi_deps
 from pharia_skill.pharia_skill_cli import PhariaSkillCli
 
 
-@pytest.fixture
-def build_skill() -> str:
-    """Ensure a built skill exists."""
-    if not os.path.exists("haiku.wasm"):
-        setup_wasi_deps()
-        run_componentize_py("examples.haiku")
-    return "haiku.wasm"
-
-
+@pytest.mark.kernel
 def test_download_pharia_skill():
     response = PhariaSkillCli.download_pharia_skill()
     assert response is not None
 
 
+@pytest.mark.kernel
 def test_pharia_skill_update_if_needed():
     PhariaSkillCli.update_if_needed()
-
-
-def test_publish_skill(build_skill: str):
-    cli = PhariaSkillCli()
-    cli.publish(build_skill)
