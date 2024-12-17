db517bf9f80226af1bacec9871bbd9a3ea4facf3
Jia Ho Lee <jiaho.lee@aleph-alpha.com>
2024-12-17 11:13:09 +0100

    test: add integration test for document search and metadata

--- scripts/run_skill.sh
+++ scripts/run_skill.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-PHARIA_AI_TOKEN=$1
-PHARIA_KERNEL_ADDRESS=${2-http://127.0.0.1:8081}
+SKILL_NAME=$1
+PHARIA_AI_TOKEN=$2
+PHARIA_KERNEL_ADDRESS=${3-http://127.0.0.1:8081}
 
 echo "Copying built skill..."
 mkdir -p skills
-cp haiku.wasm skills/haiku.wasm
+cp $SKILL_NAME.wasm skills/$SKILL_NAME.wasm
 
 echo "Waiting for skill to be available..."
 sleep 1
@@ -15,12 +16,13 @@ RESPONSE_CODE=$(curl -w '%{http_code}' -s -o output.result \
                 $PHARIA_KERNEL_ADDRESS/execute_skill \
                 -H "Authorization: Bearer $PHARIA_AI_TOKEN" \
                 -H 'Content-Type: application/json' \
-                -d '{ "skill" : "dev/haiku", "input" : "Homer" }')
+                -d "{ \"skill\" : \"dev/$SKILL_NAME\", \"input\" : \"Kernel\" }")
+
+cat output.result
 
 if [ "$RESPONSE_CODE" = "200" ]; then
     exit 0
 else
     echo "unexpected response code: RESPONSE_CODE='$RESPONSE_CODE'"
-    cat output.result
     exit 1
 fi
