ccd65d17a7aec1dd064a42b74e7c2956689db59d
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-01-16 11:22:22 +0100

    docs: specify why ipython is always activated if any tool is provided

--- pharia_skill/llama3/assistant.py
+++ pharia_skill/llama3/assistant.py
@@ -31,6 +31,15 @@ class AssistantMessage(MessageApi):
         return AssistantMessage(content=response.text)
 
     def render(self) -> str:
+        """Llama will end messages with <|eom_id|> instead of <|eot_id|> if it responds
+        with a tool call and `Environment: ipython` is set in the system prompt. If `ipython`
+        is not turned on, it will also end tool calls with <|eot_id|>.
+
+        Reference: https://www.llama.com/docs/model-cards-and-prompt-formats/llama3_1/
+
+        We always turn on `ipython` in the system prompt, so we always use `<|eom_id|>` for serialization.
+        """
         if self.tool_call is not None:
             return f"{self.role.render()}\n\n{self.tool_call.render()}{SpecialTokens.EndOfMessage.value}"
+
         return f"{self.role.render()}\n\n{self.content}{SpecialTokens.EndOfTurn.value}"
