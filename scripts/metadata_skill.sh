fcbcc8801b0711ea344d36a58bec46c855b7fe49
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-02-13 13:57:35 +0100

    ci: test skill metadata request

---
+++ scripts/metadata_skill.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SKILL_NAME=$1
+PHARIA_AI_TOKEN=$2
+PHARIA_KERNEL_ADDRESS=${3-http://127.0.0.1:8081}
+
+echo "Copying built skill..."
+mkdir -p skills
+cp $SKILL_NAME.wasm skills/$SKILL_NAME.wasm
+
+echo "Waiting for skill to be available..."
+sleep 1
+
+echo "Getting skill metadata..."
+RESPONSE_CODE=$(curl -w '%{http_code}' -s -o output.result \
+                $PHARIA_KERNEL_ADDRESS/v1/skills/dev/$SKILL_NAME/metadata \
+                -H "Authorization: Bearer $PHARIA_AI_TOKEN" \
+                -H 'Content-Type: application/json' )
+
+cat output.result
+
+if [ "$RESPONSE_CODE" = "200" ]; then
+    exit 0
+else
+    echo "unexpected response code: RESPONSE_CODE='$RESPONSE_CODE'"
+    exit 1
+fi
