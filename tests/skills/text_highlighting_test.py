fa08445e2bd60fc917ecb90937ce952668b33ace
Jia Ho Lee <jiaho.lee@aleph-alpha.com>
2025-02-19 17:29:58 +0100

    test: run text highlighting skill with DevCsi

--- tests/skills/text_highlighting_test.py
+++ tests/skills/text_highlighting_test.py
@@ -2,17 +2,15 @@
 Given a question and a fitting text found via Rag, we want to generate an answer and highlight the relevant part of the text.
 """
 
+from enum import Enum
+
 from pydantic import BaseModel
 
 from pharia_skill import CompletionParams, Csi, skill
 from pharia_skill.csi.inference import (
-    Completion,
-    FinishReason,
     Granularity,
-    TextScore,
-    TokenUsage,
 )
-from pharia_skill.testing import StubCsi
+from pharia_skill.testing.dev.csi import DevCsi
 
 
 class Input(BaseModel):
@@ -20,60 +18,74 @@ class Input(BaseModel):
     text: str
 
 
+class TextRelevancy(str, Enum):
+    HIGHLY_RELEVANT = "highly_relevant"  # text score >= 1
+    RELEVANT = "relevant"  # text score > 0
+    IRRELEVANT = "irrelevant"  # text score <= 0
+
+
+class Explanation(BaseModel):
+    start: int
+    length: int
+    relevancy: TextRelevancy
+
+
 class Output(BaseModel):
     answer: str
-    highlight_begin: int
-    highlight_end: int
+    explanations: list[Explanation]
 
 
 @skill
 def highlighting(csi: Csi, input: Input) -> Output:
-    prompt = f"""Question: {input.question}
-Text: {input.text}
-Answer:"""
-    model = "llama-3.1-8b-instruct"
+    first_prompt = f"Question: {input.question}\nText: "
+    second_prompt = f"{input.text}\nAnswer:"
+    prompt = first_prompt + second_prompt
+    model = "pharia-1-llm-7b-control"
     params = CompletionParams(max_tokens=64, return_special_tokens=False)
     completion = csi.complete(model, prompt, params)
     answer = completion.text
 
-    explanations = csi._explain(prompt=prompt, target=answer, model=model)
-    first_explanation = explanations[0]
-    highlight_begin = first_explanation.start
-    highlight_end = first_explanation.start + first_explanation.length
-
-    return Output(
-        answer=answer, highlight_begin=highlight_begin, highlight_end=highlight_end
+    explanations = csi._explain(
+        prompt=prompt,
+        target=answer,
+        model=model,
+        granularity=Granularity.SENTENCE,
     )
-
-
-class TextHighlightingStubCsi(StubCsi):
-    def complete(self, model: str, prompt: str, params: CompletionParams) -> Completion:
-        return Completion(
-            text=" Extreme conditions.",
-            finish_reason=FinishReason.STOP,
-            logprobs=[],
-            usage=TokenUsage(prompt=0, completion=0),
+    relevant_explanations = [
+        Explanation(
+            start=explanation.start - len(first_prompt),
+            length=explanation.length,
+            relevancy=TextRelevancy.HIGHLY_RELEVANT
+            if explanation.score >= 1
+            else TextRelevancy.RELEVANT,
         )
+        for explanation in explanations
+        if explanation.score > 0
+        and explanation.start >= len(first_prompt)
+        and explanation.start + explanation.length
+        <= len(first_prompt) + len(input.text)
+    ]
 
-    def _explain(
-        self,
-        prompt: str,
-        target: str,
-        model: str,
-        granularity: Granularity = Granularity.AUTO,
-    ) -> list[TextScore]:
-        return [TextScore(start=0, length=0, score=0.5)]
+    return Output(answer=answer, explanations=relevant_explanations)
 
 
 def test_run_text_highlighting_skill():
-    input = Input(
-        question="What is the ecosystem adapted to?",
-        text="""Scientists at the European Southern Observatory announced a groundbreaking discovery today: microbial life detected in the water-rich atmosphere of Proxima Centauri b, our closest neighboring exoplanet.
+    input_text = """Scientists at the European Southern Observatory announced a groundbreaking discovery today: microbial life detected in the water-rich atmosphere of Proxima Centauri b, our closest neighboring exoplanet.
 The evidence, drawn from a unique spectral signature of organic compounds, hints at an ecosystem adapted to extreme conditions.
 This finding, while not complex extraterrestrial life, significantly raises the prospects of life's commonality in the universe.
-The international community is abuzz with plans for more focused research and potential interstellar missions.""",
+The international community is abuzz with plans for more focused research and potential interstellar missions."""
+    input = Input(
+        question="What is the ecosystem adapted to?",
+        text=input_text,
     )
-    csi = TextHighlightingStubCsi()
+    csi = DevCsi()
     output = highlighting(csi, input)
 
-    assert output.answer == " Extreme conditions."
+    assert any(
+        [
+            "extreme conditions"
+            in input_text[explanation.start : explanation.start + explanation.length]
+            for explanation in output.explanations
+            if explanation.relevancy is TextRelevancy.HIGHLY_RELEVANT
+        ]
+    )
