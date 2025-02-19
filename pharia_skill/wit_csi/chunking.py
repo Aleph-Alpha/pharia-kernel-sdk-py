592b69129e1a23852821a0b20faec6679cab2efa
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-02-19 10:24:56 +0100

    refactor: prefix wit imports instead of renaming

--- pharia_skill/wit_csi/chunking.py
+++ pharia_skill/wit_csi/chunking.py
@@ -1,17 +1,16 @@
 from ..csi.chunking import ChunkParams, ChunkRequest
-from ..wit.imports.chunking import ChunkParams as WitChunkParams
-from ..wit.imports.chunking import ChunkRequest as WitChunkRequest
+from ..wit.imports import chunking as wit
 
 
-def chunk_params_to_wit(chunk_params: ChunkParams) -> WitChunkParams:
-    return WitChunkParams(
+def chunk_params_to_wit(chunk_params: ChunkParams) -> wit.ChunkParams:
+    return wit.ChunkParams(
         model=chunk_params.model,
         max_tokens=chunk_params.max_tokens,
         overlap=chunk_params.overlap,
     )
 
 
-def chunk_request_to_wit(chunk_request: ChunkRequest) -> WitChunkRequest:
-    return WitChunkRequest(
+def chunk_request_to_wit(chunk_request: ChunkRequest) -> wit.ChunkRequest:
+    return wit.ChunkRequest(
         text=chunk_request.text, params=chunk_params_to_wit(chunk_request.params)
     )
