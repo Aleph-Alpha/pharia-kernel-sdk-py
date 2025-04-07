e5772feba31e8f23d9b32a513cf6238a28a6d703
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-04-07 14:28:55 +0200

    feat: good error message when zlib is imported in skill build step

--- tests/pharia_skill_cli_test.py
+++ tests/pharia_skill_cli_test.py
@@ -3,6 +3,13 @@ from tempfile import TemporaryDirectory
 
 import pytest
 
+from pharia_skill.cli import (
+    IsMessageStream,
+    IsSkill,
+    NoHttpError,
+    SkillType,
+    run_componentize_py,
+)
 from pharia_skill.pharia_skill_cli import PhariaSkillCli
 
 
@@ -25,3 +32,33 @@ def test_download_pharia_skill_windows():
 @pytest.mark.kernel
 def test_pharia_skill_update_if_needed():
     PhariaSkillCli.update_if_needed()
+
+
+def test_building_message_stream_skill_with_wrong_skill_type_raises():
+    with pytest.raises(IsMessageStream):
+        run_componentize_py(
+            "tests.skills.streaming_haiku_chat",
+            "haiku_stream.wasm",
+            False,
+            SkillType.SKILL,
+        )
+
+
+def test_building_skill_with_wrong_skill_type_raises():
+    with pytest.raises(IsSkill):
+        run_componentize_py(
+            "tests.skills.haiku",
+            "haiku.wasm",
+            False,
+            SkillType.MESSAGE_STREAM_SKILL,
+        )
+
+
+def test_building_skill_which_imports_requests_raises():
+    with pytest.raises(NoHttpError):
+        run_componentize_py(
+            "tests.skills.http_haiku",
+            "http_haiku.wasm",
+            False,
+            SkillType.SKILL,
+        )
