12878ca3af07436fc1ae225b1046f9e17523fe09
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-06-17 17:04:15 +0200

    refactor: use cli from pypi

--- tests/pharia_skill_cli_test.py
+++ tests/pharia_skill_cli_test.py
@@ -1,6 +1,3 @@
-from pathlib import Path
-from tempfile import TemporaryDirectory
-
 import pytest
 
 from pharia_skill.cli import (
@@ -11,28 +8,6 @@ from pharia_skill.cli import (
     run_componentize_py,
     setup_wasi_deps,
 )
-from pharia_skill.pharia_skill_cli import PhariaSkillCli
-
-
-@pytest.mark.kernel
-def test_download_pharia_skill():
-    tempdir = TemporaryDirectory()
-    dir = Path(tempdir.name)
-    PhariaSkillCli.download_unix_tar(dir)
-    assert (dir / "pharia-skill-cli").exists()
-
-
-@pytest.mark.kernel
-def test_download_pharia_skill_windows():
-    tempdir = TemporaryDirectory()
-    dir = Path(tempdir.name)
-    PhariaSkillCli.download_windows_zip(dir)
-    assert (dir / "pharia-skill-cli.exe").exists()
-
-
-@pytest.mark.kernel
-def test_pharia_skill_update_if_needed():
-    PhariaSkillCli.update_if_needed()
 
 
 @pytest.fixture(autouse=True)
