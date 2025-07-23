a31c27cbdd14f12259dfc204117a8e4bfcc1a8d9
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-07-23 14:35:11 +0200

    ci: run the kernel with pharia ai related services configured

--- scripts/run_kernel.sh
+++ scripts/run_kernel.sh
@@ -12,6 +12,9 @@ mkdir -p skills
 (podman run \
   -v ./skills:/app/skills \
   -e NAMESPACE_UPDATE_INTERVAL=300ms \
+  -e INFERENCE_URL=https://inference-api.product.pharia.com \
+  -e DOCUMENT_INDEX_URL=https://document-index.product.pharia.com \
+  -e AUTHORIZATION_URL=https://pharia-iam.product.pharia.com \
   -e LOG_LEVEL="pharia_kernel=debug" \
   -e NAMESPACES__DEV__DIRECTORY="skills" \
   -p 8081:8081\
