7e11e6dd1f039a551362c4d3c39c7614780cb8ba
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-08-20 16:20:13 +0200

    feat: prototype agent decorator

--- examples/web_search.py
+++ examples/web_search.py
@@ -1,13 +1,6 @@
-from pydantic import BaseModel
+from pharia_skill import AgentInput, Csi, Message, MessageWriter, agent
 
-from pharia_skill import Csi, Message, MessageWriter, message_stream
-
-
-class Input(BaseModel):
-    messages: list[Message]
-
-
-SYSTEM = """You are a helpful research assistant.
+DEVELOPER = """You are a helpful research assistant.
 When tasked with a question, you will take as many steps and tries as needed to find the correct answer.
 If you are doing a tool call, only respond with the tool call itself.
 Do not respond with any reasoning or explanation in this case.
@@ -16,11 +9,11 @@ You can search the web and fetch content from URLs. Use markdown content when fe
 To ensure that efficiency, only fetch the content after you have found the relevant URLs."""
 
 
-@message_stream
-def web_search(csi: Csi, writer: MessageWriter[None], input: Input) -> None:
+@agent
+def web_search(csi: Csi, writer: MessageWriter[None], input: AgentInput) -> None:
     """A Skill that can decide to search the web."""
 
-    model = "llama-3.3-70b-instruct"
-    messages = [Message.system(SYSTEM), *input.messages]
-    with csi.chat_stream(model, messages, tools=["search", "fetch"]) as response:
+    model = "gpt-4o"
+    messages = [Message.developer(DEVELOPER), *input.as_chat_messages()]
+    with csi.chat_stream(model, messages, tools=["search", "scrape"]) as response:
         writer.forward_response(response)
