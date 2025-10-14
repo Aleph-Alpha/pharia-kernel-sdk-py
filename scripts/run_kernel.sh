b2e97429c8256150ac0e47276f90a09554c0d26c
Max Hammer <max.hammer@aleph-alpha.com>
2025-10-14 17:09:12 +0200

    chore: run integration tests against dev-0001 env in CI

--- scripts/run_kernel.sh
+++ scripts/run_kernel.sh
@@ -2,6 +2,7 @@
 
 GITHUB_USER=$1
 GITHUB_TOKEN=$2
+domain=${3-product.pharia.com}
 
 podman login ghcr.io -u $GITHUB_USER -p $GITHUB_TOKEN
 podman pull ghcr.io/aleph-alpha/pharia-kernel/pharia-kernel:latest
@@ -12,9 +13,9 @@ mkdir -p skills
 (podman run \
   -v ./skills:/app/skills \
   -e NAMESPACE_UPDATE_INTERVAL=300ms \
-  -e INFERENCE_URL=https://inference-api.product.pharia.com \
-  -e DOCUMENT_INDEX_URL=https://document-index.product.pharia.com \
-  -e AUTHORIZATION_URL=https://pharia-iam.product.pharia.com \
+  -e INFERENCE_URL=https://inference-api.$domain \
+  -e DOCUMENT_INDEX_URL=https://document-index.$domain \
+  -e AUTHORIZATION_URL=https://pharia-iam.$domain \
   -e LOG_LEVEL="pharia_kernel=debug" \
   -e NAMESPACES__DEV__DIRECTORY="skills" \
   -p 8081:8081\
